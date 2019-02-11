2d2df3c91289fc5e4753b6201c8b4f5f1de60f63
goodekat <kgoode@iastate.edu>
2019-02-11 16:48:19 -0600

    added Iowa tornado data

---
+++ iowa_tornados/code/cleaning_tornado_data.R
@@ -0,0 +1,24 @@
+# Cleaning tornado data
+
+# Data obtained from: http://www.tornadohistoryproject.com/tornado/Iowa/table
+# Link to variables: https://www.spc.noaa.gov/wcm/data/SPC_severe_database_description.pdf
+
+## Libraries -------------------------------------------------------------------------
+
+# Load libraries
+library(tidyverse)
+library(lubridate)
+
+## Data steps ------------------------------------------------------------------------
+
+# Read in the tornado data
+tornados <- read_csv("./iowa_tornados/data/iowa_tornado_data.csv")
+
+# Clean the tornado data
+tornados_cleaned <- tornados %>%
+  mutate(Year = year(mdy(Date))) %>%
+  filter(Year %in% c(2000:2017)) %>%
+  select(Date, Time, Segment, State1:Length, Damage)
+
+# Export the tornado data
+write_csv(tornados_cleaned, "./iowa_tornados/data/iowa_tornado_data_cleaned.csv")
