4f6b19177de84018fddfc8875d3d9a9b3a7c7e73
goodekat <kgoode@iastate.edu>
2019-02-11 17:19:47 -0600

    saved code too

--- iowa_tornados/code/cleaning_tornado_data.R
+++ iowa_tornados/code/cleaning_tornado_data.R
@@ -14,12 +14,12 @@ library(lubridate)
 # Read in the tornado data
 tornados <- read_csv("./iowa_tornados/data/iowa_tornado_data.csv")
 
-# Clean the tornado data
-tornados_cleaned <- tornados %>%
+# Clean the tornado data and extract the information I am interested in
+tornados_2000_2017 <- tornados %>%
   mutate(Year = year(mdy(Date))) %>%
   filter(Year %in% c(2000:2017), 
          Segment != "State") %>%
   select(Date, Time, State1:Length, Damage)
 
 # Export the tornado data
-write.csv(tornados_cleaned, "./iowa_tornados/data/iowa_tornado_data_cleaned.csv", row.names = FALSE)
+write.csv(tornados_2000_2017, "./iowa_tornados/data/iowa_tornados_2000_2017.csv", row.names = FALSE)
