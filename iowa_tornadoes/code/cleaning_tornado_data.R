4e23a9a7b2eaf55d29ee5398bb120c11915d333c
goodekat <kgoode@iastate.edu>
2019-02-11 20:53:07 -0600

    fixed spelling

--- iowa_tornadoes/code/cleaning_tornado_data.R
+++ iowa_tornadoes/code/cleaning_tornado_data.R
@@ -12,14 +12,14 @@ library(lubridate)
 ## Data steps ------------------------------------------------------------------------
 
 # Read in the tornado data
-tornados <- read_csv("./iowa_tornados/data/iowa_tornado_data.csv")
+tornadoes <- read_csv("./iowa_tornadoes/data/iowa_tornado_data.csv")
 
 # Clean the tornado data and extract the information I am interested in
-tornados_2000_2017 <- tornados %>%
+tornadoes_2000_2017 <- tornadoes %>%
   mutate(Year = year(mdy(Date))) %>%
   filter(Year %in% c(2000:2017), 
          Segment != "State") %>%
   select(Date, Time, Fujita:Length, Damage)
 
 # Export the tornado data
-write_csv(tornados_2000_2017, "./iowa_tornados/data/iowa_tornados_2000_2017.csv")
+write_csv(tornadoes_2000_2017, "./iowa_tornadoes/data/iowa_tornadoes_2000_2017.csv")
--- iowa_tornadoes/readme.md
+++ iowa_tornadoes/readme.md
@@ -1,3 +1,3 @@
-This folder contains data and code relating to tornados in Iowa from 1950 to 2017.
+This folder contains data and code relating to tornadoes in Iowa from 1950 to 2017.
 
 I obtained the data from [tornado history project](http://www.tornadohistoryproject.com/) which claims to have gotten their data from [NOAA](https://www.spc.noaa.gov/wcm/#data).
